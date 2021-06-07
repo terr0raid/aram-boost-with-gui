4c30aa05de92eec504bfa64519b1822087796170
cyclnxd <72807239+cyclnxd@users.noreply.github.com>
2021-06-07 14:32:11 +0300

    fixed bug
    fixed bug and code refactored

--- main.py
+++ main.py
@@ -10,45 +10,29 @@ except ModuleNotFoundError:
     exit()
 
 class AramBoostGUI(QWidget):
+
+    #constant
+    command = "WMIC PROCESS WHERE name='LeagueClientUx.exe' GET commandline"
+    url = 'https://127.0.0.1:%s/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","activateBattleBoostV1",""]'
+    checker = False
+    
     def __init__(self, title, x1, y1, x2, y2):
         super(AramBoostGUI,self).__init__()
         self.initUI(title, x1, y1, x2,y2)
 
+
+    def postProcess(self, port, password):
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.session = requests.session()
+        self.session.verify = False
+        result = self.session.post(self.url % port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+        return result
+
     def warningMessageBox(self, title, msg):
        QMessageBox.warning(self, title, msg)
-    def aboutMessageBox(self, title, msg):
-       QMessageBox.about(self, title, msg)
 
-    def boostAram(self):
-        try:        
-            command = "WMIC PROCESS WHERE name='LeagueClientUx.exe' GET commandline"
-
-            outputText = subprocess.Popen(command, stdout=subprocess.PIPE,
-                                          shell=True).stdout.read().decode('utf-8')
-
-            port = re.findall(r'"--app-port=(.*?)"', outputText)[0]
-            password = re.findall(r'"--remoting-auth-token=(.*?)"', outputText)[0]
-
-            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-            session = requests.session()
-            session.verify = False
-            while True:
-                try:
-                    result = session.post('https://127.0.0.1:%s/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","activateBattleBoostV1",""]' %
-                        port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
-                    if result.status_code == 200:
-                        self.aboutMessageBox('Basarili', 'Takviye islemi gerceklesti! Uygulama kapatiliyor.')
-                        exit()
-                    else:
-                        self.warningMessageBox('Hata', 'Bir hata olustu, oyunun acik oldugundan ve secim ekraninda oldugunuzdan emin olun! status code hatasi')
-                        exit()
-                except:
-                    self.warningMessageBox('Hata', 'Bir hata olustu, oyunun acik oldugundan ve secim ekraninda oldugunuzdan emin olun! post islemi hatasi')
-                    break
-        except:
-            self.warningMessageBox('Hata', 'Bir hata olustu, oyunun acik oldugundan ve secim ekraninda oldugunuzdan emin olun! client kapali hatasi')
-            exit()
+    def infoMessageBox(self, title, msg):
+       QMessageBox.information(self, title, msg)
 
     def createButton(self, text, toolTip, func):
         self.button = QPushButton(self)
@@ -62,8 +46,30 @@ class AramBoostGUI(QWidget):
     def initUI(self, title, x1, y1, x2, y2):
         self.setWindowTitle(title)
         self.setGeometry(x1, y1, x2, y2)
-        self.createButton("Aram Boost BGY OZEL", "Bas boostu amugagoyum", self.boostAram)
+        self.createButton("Aram Boost", "Bas boostu amugagoyum", self.boostAram)
 
+    def boostAram(self):
+        try:        
+            outputText = subprocess.Popen(self.command, stdout=subprocess.PIPE,
+                                          shell=True).stdout.read().decode('utf-8')
+
+            port = re.findall(r'"--app-port=(.*?)"', outputText)[0]
+            password = re.findall(r'"--remoting-auth-token=(.*?)"', outputText)[0]
+
+            while True:
+                if (self.checker == True):
+                    self.infoMessageBox('Basarisiz', 'Takviye islemi zaten gerceklestirildi! Gerceklesmediyse uygulamayi yeniden baslatin.')
+                    break
+                result = self.postProcess(port, password)
+                if result.status_code == 200:
+                    self.infoMessageBox('Basarili', 'Takviye islemi gerceklesti! Uygulamayi kapatabilirsiniz')
+                    self.checker = True
+                    break
+                else:
+                    self.warningMessageBox('Hata', 'Bir hata olustu, oyunun acik oldugundan ve secim ekraninda oldugunuzdan emin olun! Uygulamayi yonetici olarak calistirin.')
+                    break
+        except:
+            self.warningMessageBox('Hata', 'Bir hata olustu, lutfen oyunu baslatin.')
 
 if __name__ == '__main__':
 
